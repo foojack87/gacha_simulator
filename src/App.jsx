af7a908f6ec738d3ef0a7075191249c45b158a26
unknown <foojack@gmail.com>
2023-05-09 14:04:11 +0800

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import SeeleBanner from './components/SeeleBanner';
 import Container from './layout/Container';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 function App() {
   return (
